7a5dee017eb3db5aed13567e9a7b4b9de5b740e3
Ekaterina <Katyago2005@gmail.com>
2024-11-14 11:55:01 +0300

    Added file

--- test.py
+++ test.py
@@ -1,13 +1,21 @@
 import unittest
 
-def area(a, b):
-    return a * b
+def perimeter(a, b):
+    return 2 * (a + b)
 
-class RectangleTestCase(unittest.TestCase):
-    def test_zero_mul(self):
-        res = area(10, 0)
+class RectanglePerimeterTestCase(unittest.TestCase):
+    def test_zero_perimeter(self):
+        res = perimeter(0, 0)
         self.assertEqual(res, 0)
 
-    def test_square_mul(self):
-        res = area(10, 10)
-        self.assertEqual(res, 100)
+    def test_normal_perimeter(self):
+        res = perimeter(10, 5)
+        self.assertEqual(res, 30)
+
+    def test_square_perimeter(self):
+        res = perimeter(10, 10)
+        self.assertEqual(res, 40)
+
+if __name__ == '__main__':
+    unittest.main()
+ 
